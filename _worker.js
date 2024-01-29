c97a67ad3f724323367e29f7745624c62f8c676d
JOY <71791037+wuzhuohua168@users.noreply.github.com>
2024-01-30 01:05:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '0fe8ad89-05a4-4e90-97a4-f79bdafe8290';
 
-const proxyIPs = ['www.udemy.com'];
+const proxyIPs = ['188.42.145.57'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
